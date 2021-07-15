725320811cc1b4a793a503aa61c534fe800670ad
minhnc <minhnc62@wru.vn>
2021-07-15 08:34:50 +0700

    bt4

---
+++ web-Adv/4/hw/js/index.js
@@ -0,0 +1,17 @@
+
+
+$(function() {
+    $('.selectpicker').selectpicker();
+    $('pick__lang').selectpicker();
+});
+
+function go__bahasa() {
+    location = document.pilih__bahasa.ipicked__bahasa.
+    options[document.pilih__bahasa.ipicked__bahasa.selectedIndex].value
+}
+
+$('#myDropdown').ddslick({
+    onSelected: function(selectedData){
+        //callback function: do something with selectedData;
+    }   
+});
