4ba06aef603ae3db810048754f5ee688846b186e
minhnc <minhnc62@wru.vn>
2021-11-05 17:30:12 +0700

    update

--- project/js/script.js
+++ project/js/script.js
@@ -16,12 +16,39 @@ document.querySelector('#menu-btn').onclick = () =>{
     navbar.classList.toggle('active');
 }
 
+let showcasedt = document.querySelector('.show-case-dt');
+let showcaselaptop = document.querySelector('.show-case-laptop');
+let showcasetv = document.querySelector('.show-case-tv');
+let showcasephukien = document.querySelector('.show-case-phukien');
 
-let showcase = document.querySelector('.show-case-tv');
 
-document.querySelector('#tv').onclick = () =>{
-    showcase.classList.toggle('active');
+document.querySelector('#laptop').onclick = () =>{
+    showcaselaptop.classList.toggle('active');
+    showcasedt.classList.toggle('active-laptop');
+    
+    
 }
+document.querySelector('#tv').onclick = () =>{
+    showcasetv.classList.toggle('active');
+    showcaselaptop.classList.toggle('active-tv');
+    showcasedt.classList.toggle('active-tv');
+    
+}
+document.querySelector('#phukien').onclick = () =>{
+    showcasephukien.classList.toggle('active');
+    showcasetv.classList.toggle('active-phukien');
+    showcaselaptop.classList.toggle('active-phukien');
+    showcasedt.classList.toggle('active-phukien');
+    
+}
+
+$('.list-2').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    
+  });
 
 
 
