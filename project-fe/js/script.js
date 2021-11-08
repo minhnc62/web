f9af427c931d30680ef433b13dfd09a4a0a4f363
minhnc <minhnc62@wru.vn>
2021-11-08 12:48:41 +0700

    home

--- project-fe/js/script.js
+++ project-fe/js/script.js
@@ -1,9 +1,4 @@
 
-
-
-
-
-
 // ------------------
 
 $(document).ready(function () {
@@ -34,12 +29,34 @@ $(document).ready(function () {
 // -----------------------------------------
 
 
+var swiper = new Swiper(".banner-slide", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  loop: true,
+  autoplay: {
+    delay: 6500,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 
 
 
 
-
-
+var swiper = new Swiper(".pk-slider", {
+  slidesPerView: 4,
+  
+  spaceBetween: 30,
+  freeMode: true,
+  
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
 
 
 
