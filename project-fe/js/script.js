a6c8ea04857edfa150000d55a518807c17d0a496
minhnc <minhnc62@wru.vn>
2021-11-13 17:08:05 +0700

    update-chitiet

--- project-fe/js/script.js
+++ project-fe/js/script.js
@@ -90,6 +90,26 @@ var swiper = new Swiper(".banner-store-slider", {
   centeredSlides: true,
   loop: true,
 });
+// -----------------------------------------------------------------------------------------------
+
+$('input.input-qty').each(function() {
+  var $this = $(this),
+    qty = $this.parent().find('.is-form'),
+    min = Number($this.attr('min')),
+    max = Number($this.attr('max'))
+  if (min == 0) {
+    var d = 0
+  } else d = min
+  $(qty).on('click', function() {
+    if ($(this).hasClass('minus')) {
+      if (d > min) d += -1
+    } else if ($(this).hasClass('plus')) {
+      var x = Number($this.val()) + 1
+      if (x <= max) d += 1
+    }
+    $this.attr('value', d).val(d)
+  })
+})
      
 
 
@@ -118,6 +138,25 @@ var swiper = new Swiper(".banner-store-slider", {
 
 
 
+var swiper = new Swiper(".mySwiper", {
+  loop: true,
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+var swiper2 = new Swiper(".mySwiper2", {
+  loop: true,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: swiper,
+  },
+});
+ 
 
 
 
