5e6f846a80242f87b8af2ff56eb7cc7f9a74dafe
minhnc <minhnc62@wru.vn>
2021-11-25 17:39:10 +0700

    update

--- project-fe/js/header.js
+++ project-fe/js/header.js
@@ -1,6 +1,6 @@
 let searchForm = document.querySelector('.search-form');
 let searchBtn = document.querySelector('#search-btn')
-let shoppingCart = document.querySelector('.shopping-cart');
+
 let navbar = document.querySelector('.navbar');
 let menuBtn = document.querySelector('#menu-btn')
 
@@ -8,24 +8,21 @@ document.querySelector('#search-btn').onclick = () =>{
     searchForm.classList.toggle('active');
 }
 
-searchBtn.addEventListener('click', ()=>{
-    searchBtn.classList.toggle('fa-times');
-})
 
 
 
-document.querySelector('#cart-btn').onclick = () =>{
-    shoppingCart.classList.toggle('active');
-}
 
 
 
-document.querySelector('#menu-btn').onclick = () =>{
-    navbar.classList.toggle('active');
-}
+
+
+// document.querySelector('#menu-btn').onclick = () =>{
+//     navbar.classList.toggle('active');
+// }
 
 menuBtn.addEventListener('click', ()=>{
     menuBtn.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
 })
 
 
